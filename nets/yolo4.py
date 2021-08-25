bb1883897b5593d8f6f9dda361e8fddce7728e7c
chenjl <chenjl@augurit.com>
2021-08-25 10:12:21 +0800

    优化

--- nets/yolo4.py
+++ nets/yolo4.py
@@ -147,14 +147,6 @@ def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
                     [grid_shape[0], 1, 1, 1])
     grid = K.concatenate([grid_x, grid_y])
     grid = K.cast(grid, K.dtype(feats))
-
-    #---------------------------------------------------#
-    #   将预测结果调整成(batch_size,13,13,3,85)
-    #   85可拆分成4 + 1 + 80
-    #   4代表的是中心宽高的调整参数
-    #   1代表的是框的置信度
-    #   80代表的是种类的置信度
-    #---------------------------------------------------#
     feats = K.reshape(feats, [-1, grid_shape[0], grid_shape[1], num_anchors, num_classes + 5])
 
     #---------------------------------------------------#
@@ -205,24 +197,8 @@ def yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape):
     return boxes
 
 
-#---------------------------------------------------#
-#   获取每个box和它的得分
-#---------------------------------------------------#
 def yolo_boxes_and_scores(feats, anchors, num_classes, input_shape, image_shape, letterbox_image):
-    #-----------------------------------------------------------------#
-    #   将预测值调成真实值
-    #   box_xy : -1,13,13,3,2; 
-    #   box_wh : -1,13,13,3,2; 
-    #   box_confidence : -1,13,13,3,1; 
-    #   box_class_probs : -1,13,13,3,80;
-    #-----------------------------------------------------------------#
     box_xy, box_wh, box_confidence, box_class_probs = yolo_head(feats, anchors, num_classes, input_shape)
-    #-----------------------------------------------------------------#
-    #   在图像传入网络预测前会进行letterbox_image给图像周围添加灰条
-    #   因此生成的box_xy, box_wh是相对于有灰条的图像的
-    #   我们需要对齐进行修改，去除灰条的部分。
-    #   将box_xy、和box_wh调节成y_min,y_max,xmin,xmax
-    #-----------------------------------------------------------------#
     if letterbox_image:
         boxes = yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape)
     else:
@@ -240,9 +216,7 @@ def yolo_boxes_and_scores(feats, anchors, num_classes, input_shape, image_shape,
             box_maxes[..., 0:1] * image_shape[0],  # y_max
             box_maxes[..., 1:2] * image_shape[1]  # x_max
         ])
-    #-----------------------------------------------------------------#
-    #   获得最终得分和框的位置
-    #-----------------------------------------------------------------#
+
     boxes = K.reshape(boxes, [-1, 4])
     box_scores = box_confidence * box_class_probs
     box_scores = K.reshape(box_scores, [-1, num_classes])
@@ -265,12 +239,6 @@ def yolo_eval(yolo_outputs,
         #   获得特征层的数量，有效特征层的数量为3
         #---------------------------------------------------#
         num_layers = len(yolo_outputs)
-
-    #-----------------------------------------------------------#
-    #   13x13的特征层对应的anchor是[142, 110], [192, 243], [459, 401]
-    #   26x26的特征层对应的anchor是[36, 75], [76, 55], [72, 146]
-    #   52x52的特征层对应的anchor是[12, 16], [19, 36], [40, 28]
-    #-----------------------------------------------------------#
     anchor_mask = [[6, 7, 8], [3, 4, 5], [0, 1, 2]]
     input_shape = K.shape(yolo_outputs[0])[1:3] * 32
     boxes = []
@@ -283,34 +251,20 @@ def yolo_eval(yolo_outputs,
     boxes = K.concatenate(boxes, axis=0)
     box_scores = K.concatenate(box_scores, axis=0)
 
-    
+    mask = box_scores >= score_threshold
     max_boxes_tensor = K.constant(max_boxes, dtype='int32')
     boxes_ = []
     scores_ = []
     classes_ = []
     for c in range(num_classes):
-
-        # 对小目标的阈值进行调整
-        if c =='0':
-            mask = box_scores >= score_threshold
-        else:
-            mask = box_scores >= score_threshold
-
         class_boxes = tf.boolean_mask(boxes, mask[:, c])
         class_box_scores = tf.boolean_mask(box_scores[:, c], mask[:, c])
 
-        #-----------------------------------------------------------#
-        #   非极大抑制
-        #   保留一定区域内得分最大的框
-        #-----------------------------------------------------------#
+        # tf.image.non_max_suppression 非极大值抑制
         nms_index = tf.image.non_max_suppression(
             class_boxes, class_box_scores, max_boxes_tensor, iou_threshold=iou_threshold)
 
-        #-----------------------------------------------------------#
-        #   获取非极大抑制后的结果
-        #   下列三个分别是
-        #   框的位置，得分与种类
-        #-----------------------------------------------------------#
+        # K.gather：进行索引
         class_boxes = K.gather(class_boxes, nms_index)
         class_box_scores = K.gather(class_box_scores, nms_index)
         classes = K.ones_like(class_box_scores, 'int32') * c
