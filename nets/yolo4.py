3e6271fe493ed1659546f4a31ad74647b0aa3270
chenjl <chenjl@augurit.com>
2021-09-02 11:47:59 +0800

    add yolotinu and attention

--- nets/yolo4.py
+++ nets/yolo4.py
@@ -13,8 +13,16 @@ from tensorflow.keras.regularizers import l2
 from utils.utils import compose
 
 from nets.CSPdarknet53 import darknet_body
+from nets.attention import cbam_block, eca_block, se_block
+
+attention_block = [se_block, cbam_block, eca_block]
 
 
+#--------------------------------------------------#
+#   单次卷积DarknetConv2D
+#   如果步长为2则自己设定padding方式。
+#   测试中发现没有l2正则化效果更好，所以去掉了l2正则化
+#--------------------------------------------------#
 @wraps(Conv2D)
 def DarknetConv2D(*args, **kwargs):
     # darknet_conv_kwargs = {'kernel_regularizer': l2(5e-4)}
@@ -23,9 +31,6 @@ def DarknetConv2D(*args, **kwargs):
     darknet_conv_kwargs.update(kwargs)
     return Conv2D(*args, **darknet_conv_kwargs)
 
-#---------------------------------------------------#
-#   DarknetConv2D + BatchNormalization + LeakyReLU
-#---------------------------------------------------#
 def DarknetConv2D_BN_Leaky(*args, **kwargs):
     no_bias_kwargs = {'use_bias': False}
     no_bias_kwargs.update(kwargs)
@@ -34,7 +39,9 @@ def DarknetConv2D_BN_Leaky(*args, **kwargs):
         BatchNormalization(),
         LeakyReLU(alpha=0.1))
 
+
 def make_five_convs(x, num_filters):
+    # 五次卷积
     x = DarknetConv2D_BN_Leaky(num_filters, (1, 1))(x)
     x = DarknetConv2D_BN_Leaky(num_filters * 2, (3, 3))(x)
     x = DarknetConv2D_BN_Leaky(num_filters, (1, 1))(x)
@@ -45,7 +52,9 @@ def make_five_convs(x, num_filters):
 #---------------------------------------------------#
 #   Panet网络的构建，并且获得预测结果
 #---------------------------------------------------#
-def yolo_body(inputs, num_anchors, num_classes):
+def yolo_body(inputs, num_anchors, num_classes, phi=1):
+    if phi >=4:
+        raise AssertionError("Phi must be less than or equal to 3. ")
     #---------------------------------------------------#   
     #   生成CSPdarknet53的主干模型
     #   获得三个有效特征层，他们的shape分别是：
@@ -54,6 +63,11 @@ def yolo_body(inputs, num_anchors, num_classes):
     #   13,13,1024
     #---------------------------------------------------#
     feat1, feat2, feat3 = darknet_body(inputs)
+    if 1<=phi and phi<=3:
+        feat1 = attention_block[phi-1](feat1, name='feat1')
+        feat2 = attention_block[phi-1](feat2, name='feat2')
+        feat3 = attention_block[phi-1](feat3, name='feat3')
+    
 
     # 13,13,1024 -> 13,13,512 -> 13,13,1024 -> 13,13,512 -> 13,13,2048 -> 13,13,512 -> 13,13,1024 -> 13,13,512
     P5 = DarknetConv2D_BN_Leaky(512, (1, 1))(feat3)
@@ -70,6 +84,9 @@ def yolo_body(inputs, num_anchors, num_classes):
 
     # 13,13,512 -> 13,13,256 -> 26,26,256
     P5_upsample = compose(DarknetConv2D_BN_Leaky(256, (1,1)), UpSampling2D(2))(P5)
+    if 1<=phi and phi<=3:
+        P5_upsample = attention_block[phi-1](P5_upsample, name='P5_upsample')
+
     # 26,26,512 -> 26,26,256
     P4 = DarknetConv2D_BN_Leaky(256, (1,1))(feat2)
     # 26,26,256 + 26,26,256 -> 26,26,512
@@ -80,6 +97,8 @@ def yolo_body(inputs, num_anchors, num_classes):
 
     # 26,26,256 -> 26,26,128 -> 52,52,128
     P4_upsample = compose(DarknetConv2D_BN_Leaky(128, (1,1)), UpSampling2D(2))(P4)
+    if 1<=phi and phi<=3:
+        P4_upsample = attention_block[phi-1](P4_upsample, name='P4_upsample')
     # 52,52,256 -> 52,52,128
     P3 = DarknetConv2D_BN_Leaky(128, (1,1))(feat1)
     # 52,52,128 + 52,52,128 -> 52,52,256
@@ -127,7 +146,6 @@ def yolo_body(inputs, num_anchors, num_classes):
 
     return Model(inputs, [P5_output, P4_output, P3_output])
 
-
 def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
     num_anchors = len(anchors)
     #---------------------------------------------------#
@@ -147,30 +165,25 @@ def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
                     [grid_shape[0], 1, 1, 1])
     grid = K.concatenate([grid_x, grid_y])
     grid = K.cast(grid, K.dtype(feats))
+
     feats = K.reshape(feats, [-1, grid_shape[0], grid_shape[1], num_anchors, num_classes + 5])
 
-    #---------------------------------------------------#
-    #   将预测值调成真实值
-    #   box_xy对应框的中心点
-    #   box_wh对应框的宽和高
-    #---------------------------------------------------#
     box_xy = (K.sigmoid(feats[..., :2]) + grid) / K.cast(grid_shape[...,::-1], K.dtype(feats))
     box_wh = K.exp(feats[..., 2:4]) * anchors_tensor / K.cast(input_shape[...,::-1], K.dtype(feats))
     box_confidence = K.sigmoid(feats[..., 4:5])
     box_class_probs = K.sigmoid(feats[..., 5:])
 
+    #---------------------------------------------------------------------#
+    #   在计算loss的时候返回grid, feats, box_xy, box_wh
+    #   在预测的时候返回box_xy, box_wh, box_confidence, box_class_probs
+    #---------------------------------------------------------------------#
     if calc_loss == True:
         return grid, feats, box_xy, box_wh
     return box_xy, box_wh, box_confidence, box_class_probs
 
 
-#---------------------------------------------------#
-#   对box进行调整，使其符合真实图片的样子
-#---------------------------------------------------#
+
 def yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape):
-    #-----------------------------------------------------------------#
-    #   把y轴放前面是因为方便预测框和图像的宽高进行相乘
-    #-----------------------------------------------------------------#
     box_yx = box_xy[..., ::-1]
     box_hw = box_wh[..., ::-1]
     
@@ -199,6 +212,7 @@ def yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape):
 
 def yolo_boxes_and_scores(feats, anchors, num_classes, input_shape, image_shape, letterbox_image):
     box_xy, box_wh, box_confidence, box_class_probs = yolo_head(feats, anchors, num_classes, input_shape)
+    
     if letterbox_image:
         boxes = yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape)
     else:
@@ -216,12 +230,17 @@ def yolo_boxes_and_scores(feats, anchors, num_classes, input_shape, image_shape,
             box_maxes[..., 0:1] * image_shape[0],  # y_max
             box_maxes[..., 1:2] * image_shape[1]  # x_max
         ])
-
+    #-----------------------------------------------------------------#
+    #   获得最终得分和框的位置
+    #-----------------------------------------------------------------#
     boxes = K.reshape(boxes, [-1, 4])
     box_scores = box_confidence * box_class_probs
     box_scores = K.reshape(box_scores, [-1, num_classes])
     return boxes, box_scores
 
+# ---------------------------------------------------#
+#   图片预测
+# ---------------------------------------------------#
 def yolo_eval(yolo_outputs,
               anchors,
               num_classes,
@@ -235,36 +254,37 @@ def yolo_eval(yolo_outputs,
         image_shape = K.reshape(yolo_outputs[-1],[-1])
         num_layers = len(yolo_outputs)-1
     else:
-        #---------------------------------------------------#
-        #   获得特征层的数量，有效特征层的数量为3
-        #---------------------------------------------------#
         num_layers = len(yolo_outputs)
+
     anchor_mask = [[6, 7, 8], [3, 4, 5], [0, 1, 2]]
     input_shape = K.shape(yolo_outputs[0])[1:3] * 32
     boxes = []
     box_scores = []
+
     for l in range(num_layers):
         _boxes, _box_scores = yolo_boxes_and_scores(yolo_outputs[l], anchors[anchor_mask[l]], num_classes, input_shape,
                                                     image_shape, letterbox_image)
         boxes.append(_boxes)
         box_scores.append(_box_scores)
+    #-----------------------------------------------------------#
+    #   将每个特征层的结果进行堆叠
+    #-----------------------------------------------------------#
     boxes = K.concatenate(boxes, axis=0)
     box_scores = K.concatenate(box_scores, axis=0)
-
     mask = box_scores >= score_threshold
     max_boxes_tensor = K.constant(max_boxes, dtype='int32')
     boxes_ = []
     scores_ = []
     classes_ = []
     for c in range(num_classes):
+        # 对小目标类别的阈值调低一些
         class_boxes = tf.boolean_mask(boxes, mask[:, c])
         class_box_scores = tf.boolean_mask(box_scores[:, c], mask[:, c])
 
-        # tf.image.non_max_suppression 非极大值抑制
+        # tensorflow非极大值抑制：https://www.tensorflow.org/api_docs/python/tf/image/non_max_suppression
         nms_index = tf.image.non_max_suppression(
             class_boxes, class_box_scores, max_boxes_tensor, iou_threshold=iou_threshold)
-
-        # K.gather：进行索引
+        # K.gather 检索张量reference中索引indices的元素
         class_boxes = K.gather(class_boxes, nms_index)
         class_box_scores = K.gather(class_box_scores, nms_index)
         classes = K.ones_like(class_box_scores, 'int32') * c
